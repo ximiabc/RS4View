12b2a29f8eb7d07847ae8cf7876e5aee493f8025
hao-have-no <932537602@qq.com>
2017-05-18 15:44:12 +0800

    index.html
    index主页基本完善完毕

---
+++ RS/RS2.0/js/tab.js
@@ -0,0 +1,8 @@
+			$(document).ready(function() {
+		//下拉列表		
+				$(".rs-summary").click(function() {
+					$(this).toggleClass("active");
+					$(this).next("div").slideToggle("slow").siblings(".rs-detailed").slideUp("slow");
+				    $(this).siblings(".rs-summary").removeClass("active");
+				});
+             });
