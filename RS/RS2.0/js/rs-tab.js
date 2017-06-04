d0f6e3444a75463186069cc7ef02975b15223cb2
LMJava <372265655@qq.com>
2017-06-04 19:59:01 +0800

    school-index下的ajaxget请求
    1.index侧栏审核参数改进
    2.rs-tab.js改进
    3.school-index的get请求

--- RS/RS2.0/js/rs-tab.js
+++ RS/RS2.0/js/rs-tab.js
@@ -1,15 +1,10 @@
 $(document).ready(function() {
 	//下拉列表	
 	$(".rs-summary").click(function() {
+		$('.rs-active').removeClass("rs-active");
 		$(this).toggleClass("rs-active");
 		$(this).next("div").slideToggle("slow").siblings(".rs-detailed").slideUp("slow");
-	    $(this).siblings(".rs-summary").removeClass("rs-active");
 	});
-	
-	$(".special").click(function() {
-		$(this).siblings(".rs-detailed").slideUp("slow");
-	});
-	
 	$(".rs-detailed").on('click','a',function() {
 		$('.rs-focus').removeClass("rs-focus");
 		$(this).addClass("rs-focus");
